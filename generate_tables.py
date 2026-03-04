51b67b6d84286929852f46b8248a826514052344
Gentry Atkinson <gatkinso@stedwards.edu>
2026-03-04 17:34:06 -0600

    more charts

--- generate_tables.py
+++ generate_tables.py
@@ -28,6 +28,18 @@ benefits_of_ai = {
     'Eliminating Cash Bail' : 7
 }
 
+is_ml_used = {
+    "Yes" : 40,
+    "No" :14
+}
+
+ai_tools_used = {
+    "Random Forest" : 6,
+    "Logistic Regression"	: 6,
+    "SVMs" : 3,
+    "Gradient Boosting" : 3
+}
+
 benefits_of_ai = dict(sorted(benefits_of_ai.items(), key=lambda item: item[1], reverse=True))
 
 def generate_top_six_ras_pie_chart():
@@ -60,9 +72,41 @@ def generate_benefits_of_ai_bar_chart():
     plt.tight_layout()
     plt.savefig('tables/benefits_of_ai_bar_chart.png')
 
+def generte_is_ml_used_pid_chart():
+    plt.figure(figsize=(5, 5))
+    plt.pie(
+        is_ml_used.values(), 
+        labels=is_ml_used.keys(), 
+        autopct='%.0f%%', 
+        startangle=90,
+        colors=sns.color_palette('Pastel1', n_colors=len(top_six_risk_assessments)),
+        wedgeprops={'edgecolor': 'gray'}
+    )
+    plt.title('Is ML Used for Risk Assessment', pad=20)
+    plt.axis('equal')  # Equal aspect ratio ensures that pie chart is circular.
+    plt.tight_layout()
+    plt.savefig('tables/is_ml_used.png')
+
+def generate_ai_tools_bar_chart():
+    plt.figure(figsize=(8, 6))
+    sns.barplot(x=list(ai_tools_used.keys()), y=list(ai_tools_used.values()))
+    plt.xticks(rotation=45, ha='right')
+    plt.title('Count of AI Tools Used in Experiments', pad=20)
+    # plt.xlabel('Benefit')
+    # plt.ylabel('Number of Studies Mentioning Benefit')
+    # place a numerical value above each bar
+    for index, value in enumerate(ai_tools_used.values()):
+        plt.text(index, value + 0.5, str(value), ha='center', va='bottom')
+    # set the y-axis range to be slightly higher than the maximum value for better visualization
+    plt.ylim(0, max(ai_tools_used.values()) + 2)
+    plt.tight_layout()
+    plt.savefig('tables/ai_tools_used_bar_chart.png')
+
 if __name__ == "__main__":
     sns.set_style("whitegrid")
     sns.set_palette('Greys')
     sns.set_theme(font_scale=1.5) 
     generate_top_six_ras_pie_chart()
     generate_benefits_of_ai_bar_chart()
+    generte_is_ml_used_pid_chart()
+    generate_ai_tools_bar_chart()
